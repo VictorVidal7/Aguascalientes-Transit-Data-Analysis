02d5911715e47a958fdbe60fbd88a23fac523258
Victor Vidal <victor_vdu@hotmail.com>
2024-05-04 21:55:45 -0600

    Creation of clean_data.py script to impute missing values in important columns

---
+++ clean_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load data from 'stops.txt' file
+stops = pd.read_csv('aguascalientes_gtfs/stops.txt')
+
+# Drop columns with too many missing values
+stops_clean = stops.drop(columns=['zone_id', 'stop_url', 'location_type', 'parent_station', 'stop_desc', 'stop_timezone', 'level_id', 'platform_code'])
+
+# Impute missing values in important columns (if applicable)
+stops_clean['stop_name'] = stops_clean['stop_name'].fillna('Unknown')
+
+# Save the cleaned data
+stops_clean.to_csv('aguascalientes_gtfs/stops_clean.csv', index=False)
